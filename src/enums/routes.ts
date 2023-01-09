c5d2aebf46aa26335a5a1f43e585bfdc7c62b6b7
Tihomir Selak <tknox.dr@gmail.com>
2023-01-09 02:42:34 +0100

    EVU-05: Add links to quiz

--- src/enums/routes.ts
+++ src/enums/routes.ts
@@ -5,3 +5,8 @@ export const ROUTES = {
   SUMMARY: "/summary",
   NOT_ROUND: "*",
 };
+
+export const EXTERNAL_LINKS = {
+  HALL_OF_FAME: "http://localhost:7894",
+  ABOUT: "http://localhost:7894/about",
+};
